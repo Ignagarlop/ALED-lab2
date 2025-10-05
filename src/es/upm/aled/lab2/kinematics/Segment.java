af8c8f4c4d02505b74d248446dd08c6dd589d30e
Ignagarlop <i.glopez@alumnos.upm.es>
2025-10-05 18:46:07 +0200

    Práctica 2

--- src/es/upm/aled/lab2/kinematics/Segment.java
+++ src/es/upm/aled/lab2/kinematics/Segment.java
@@ -1,6 +1,44 @@
 package es.upm.aled.lab2.kinematics;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 // TODO: Implemente la clase
+
 public class Segment {
+	private double length;          
+    private double angle;           
+    private final List<Segment> children;
+
+    
+    public Segment(double length, double angle) {
+        this.length = length;
+        this.angle = angle;
+        this.children = new ArrayList<>();
+    }
+
+    public Segment() {
+        this(0.0, 0.0);
+    }
+
+    
+    public double getLength() { return length; }
+    public double getAngle()  { return angle;  }
+    public List<Segment> getChildren() { return children; }
+
+    // Setters
+    public void setLength(double length) { this.length = length; }
+    public void setAngle(double angle)   { this.angle = angle;   }
+
+    
+    public void addChild(Segment child) {
+        if (!children.contains(child)) {
+            children.add(child);
+        }
+    }
+
+    @Override public String toString() {
+        return "Segment{length=" + length + ", angle=" + angle +
+               ", children=" + children.size() + "}";
+    }
 
 }
